37a0810354a1d624966e42efde889effcfc956dc
Sofiya Tepikin <sofiya@noa.one>
2018-03-28 17:32:43 +0200

    Styles adoptations to mobile resolutions

--- src/components/QuestionnaireSummary.js
+++ src/components/QuestionnaireSummary.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
+import './QuestionnaireSummary.css';
+
 class QuestionnaireSummary extends Component {
+
+  onStartOver() {
+    localStorage.clear();
+    window.location.reload();
+  }
 
   render() {
 
@@ -8,11 +15,15 @@ class QuestionnaireSummary extends Component {
 
     return (
       <div className="QuestionnaireSummary">
-        {questions.map(({question}, i) => <div key={i}>
+        {questions.map(({question}, i) => <div className="QuestionnaireSummary__Item" key={i}>
             <p>{question}</p>
             <p><strong>{answers[i]}</strong></p>
           </div>
         )}
+        <p><button
+          onClick={this.onStartOver}
+          className="Button">Start over
+        </button></p>
       </div>
     );
   }
